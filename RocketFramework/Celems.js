92ebd87150d2a3abe10c3029503dc61bfdd53dfb
avirt <avirtgd@gmail.com>
2025-07-09 01:56:32 +0300

    sdfsdfsf

--- RocketFramework/Celems.js
+++ RocketFramework/Celems.js
@@ -13,6 +13,8 @@ function getRaw(path_) {
 }
 
 // Обработка кастомных элементов
+
+// You can name your celems on any language!
 function celem(page_content, name, params, content) {
     // ELEMENTS
     if (name === "testText") {
@@ -21,6 +23,18 @@ function celem(page_content, name, params, content) {
 
     if (name === "bigText") {
         return page_content.replaceAll(`!${name}!`, `<p style="font-size: 60px;">${content}</p>`);
+    }
+
+    if (name === "КрасиваяКнопка") {
+        let styles = `
+        font-size: 20px;
+        border-radius: 0.5vh;
+        border: none;
+        padding: 10px;
+        background-color: rgb(29, 29, 29);
+        color: white;
+        `;
+        return page_content.replaceAll(`!${name}!`, `<button style="${styles}">${content}</button>`);
     }
 
     // STYLES
@@ -52,6 +66,7 @@ function customElementsLoader(page_content) {
     // CELEMS
     result = celem(result, "testText", 'style="font-size: 60px;"', "Text2");
     result = celem(result, "bigText", '', 'Big Text Element');
+    result = celem(result, "КрасиваяКнопка", '', 'Это тест');
 
     // STYLES
     result = celem(result, "stylesMain", '', '');
