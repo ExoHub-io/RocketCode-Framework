0388d5e0c6ebcd19115544ee866bd8ff760f14b8
avirt <avirtgd@gmail.com>
2025-07-10 05:19:04 +0300

    fix

--- routes/router.js
+++ routes/router.js
@@ -25,4 +25,10 @@ router.post('/data', (req, res) => {
   res.send(`Getted name: ${name}`);
 });
 
-module.exports = router;
+module.exports = router;axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log('GET response:', response.data);
+  })
+  .catch(error => {
+    console.error('GET error:', error);
+  });
